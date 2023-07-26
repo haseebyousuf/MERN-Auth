9c5d1bc7f0dbee40c0b772f56056adb6832e66e8
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-26 19:19:51 +0530

    add db connection

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
-
+connectDB();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
