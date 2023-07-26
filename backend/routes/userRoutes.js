2c94475d9f26e7e3e35b025d04cce8b100c642f8
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-26 19:21:12 +0530

    combine /profile route for get and put

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -12,7 +12,6 @@ import {
 router.post("/auth", authUser);
 router.post("/register", registerUser);
 router.post("/logout", logoutUser);
-router.get("/profile", getUserProfile);
-router.put("/profile", updateUserProfile);
+router.route("/profile").get(getUserProfile).put(updateUserProfile);
 
 export default router;
