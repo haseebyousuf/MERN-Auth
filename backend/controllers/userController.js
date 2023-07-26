8f6cbc8533e0cf07e5d8e6e88e42b6b81a524069
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-26 19:20:31 +0530

    add registerUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 //@desc     Auth user/set token
 //@route    POST /api/users/auth
@@ -11,7 +13,27 @@ export const authUser = asyncHandler(async (req, res) => {
 //@route    POST /api/users/register
 //@access   Public
 export const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "registerUser" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({ name, email, password });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user Data");
+  }
 });
 
 //@desc     logout user
