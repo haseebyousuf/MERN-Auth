e7a6bb4d38830d77b78302fc29c2ef833ffad924
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-27 17:43:33 +0530

    add updatePUserProfile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -75,5 +75,22 @@ export const getUserProfile = asyncHandler(async (req, res) => {
 //@route    PUT /api/users/profile
 //@access   private
 export const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "updateusrr profilte" });
+  const user = User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await User.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });
