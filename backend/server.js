9d7023ea6a0b564fce947662ad9291878ddf1e45
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-27 14:55:43 +0530

    use cookie-parser

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
@@ -10,6 +11,7 @@ const port = process.env.PORT || 5000;
 connectDB();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
