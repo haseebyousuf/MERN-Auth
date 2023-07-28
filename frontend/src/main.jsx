f9bf2f65900a547852b03bd3cdbac3c3a65b0de8
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-28 12:23:38 +0530

    configure react router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
-
+import HomeScreen from "./screens/Homescreen.jsx";
+import LoginScreen from "./screens/LoginScreen.jsx";
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
+    </Route>
+  )
+);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
