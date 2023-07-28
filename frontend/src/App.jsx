0ed1b9ea3ad15d3820ad723ef6b7fa7b8ed071f9
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-07-28 18:32:58 +0530

    add toast container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Header from "./components/Header";
 const App = () => {
   return (
     <>
       <Header />
+      <ToastContainer />
       <div className='my-4'>
         <Outlet />
       </div>
